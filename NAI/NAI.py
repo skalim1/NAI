b1d64801a05e57d10617aeab576bc894601b971f
skalim1 <skalim1@wp.pl>
2016-02-02 11:05:03 +0100

    Pętla obsługi kamerki, wyświetlanie obrazu z kamerki, obrazu szarego

--- NAI/NAI.py
+++ NAI/NAI.py
@@ -1,2 +1,15 @@
-import cv2 
+﻿import cv2 
 import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    ret, frame = cap.read()
+
+    cv2.imshow("image", frame)
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGRq2GRAY)
+#    gray = cv2.GaussianBlur(gray, (3, 3), 0)
+    small = cv2.resize(gray, (0, 0), fx=0.5, fy=0.5)
+
+    cv2.imshow('frame', gray)
