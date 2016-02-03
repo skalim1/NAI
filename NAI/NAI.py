ece6f397087bf10e0f3c6819c12a18d1245b30ca
skalim1 <skalim1@wp.pl>
2016-02-03 10:50:17 +0100

    Korekty kodu

--- NAI/NAI.py
+++ NAI/NAI.py
@@ -1,30 +1,35 @@
-﻿import cv2 
-import numpy as np
+﻿import numpy as np
+import cv2 
 
 cap = cv2.VideoCapture(0)
 
 while(True):
+
     ret, frame = cap.read()
 
-    cv2.imshow("image", frame)
+#    cv2.imshow("image", frame)
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGRq2GRAY)
-#    gray = cv2.GaussianBlur(gray, (3, 3), 0)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (3, 3), 0)
     small = cv2.resize(gray, (0, 0), fx=0.5, fy=0.5)
 
     cv2.imshow('frame', gray)
 
     edged = cv2.Canny(gray, 10, 250)
-    cv2.imshow("Edged", edged)
+#    cv2.imshow("Edged", edged)
 
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
     closed = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
-    cv2.imshow("Closed", closed) 
+#    cv2.imshow("Closed", closed) 
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cv2.imwrite("test.jpg", closed)
+image = cv2.imread("test.jpg")
+
+(d, cnts, _) = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+total = 0
 
 for c in cnts:
 	peri = cv2.arcLength(c, True)
